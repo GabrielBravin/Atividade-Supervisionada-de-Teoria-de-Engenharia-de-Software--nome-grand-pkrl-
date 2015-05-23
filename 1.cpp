8144eb57bfb04a94cba40274d34ccd1952df8f95
GabrielBravin <gabrielbravin89@gmail.com>
2015-05-23 19:39:13 -0300

    integrantes 3
    Eu ti vi se sentindo excluido

--- 1.cpp
+++ 1.cpp
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <locale.h>
 
-	integrantes: Guilherme, Hullick
+	integrantes: Guilherme, Hullick, Biel
 
 float potencia (int base, int expoente){
     int resultado = 1;
